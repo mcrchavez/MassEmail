8754363af23e381bc859e3c6a319bcf3ab54ecc5
mcrchavez <75552543+mcrchavez@users.noreply.github.com>
2020-12-06 11:43:55 -0500

    Add files via upload
    The base file for the mass emailing script v.1

---
+++ MassEmail.py
@@ -0,0 +1,150 @@
+#fucking around and shit
+
+import email , smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+
+
+#MIMEMultipart object has sub classes with different media types
+
+"""
+MIMEText — Plain text, this will make up the body of our email.
+MIMEImage — Allows us to add images to our email.
+MIMEAudio — Audio files are added with this part.
+MIMEApplication — Any other attachments can be added with this.
+"""
+
+
+class Email:
+    def __init__(self, email_addr='', email_domain='@gmail.com', passw= '', attrib = None):
+        self.email_addr = email_addr
+        self.email_domain = email_domain 
+        self.passw = passw
+        self.attrib = attrib
+
+
+    def characteristics(self):
+        return_dict = dict()
+        return_dict['Email:'] = str(self.email_addr + self.email_domain)
+        return_dict['Password:'] = str(self.passw)
+        return_dict['Attributes:'] = self.attrib
+        return return_dict
+    def __str__(self):
+        return_str = '-'*35 + '\n'
+        vals = self.characteristics()
+        
+        for key, val in vals.items():
+            if val != None:
+                return_str += str(key) + ' ' + str(val)
+            else:
+                return_str += str(key) + ' ' +  'N/A'
+            return_str += '\n'
+        return_str += '-'*35 + '\n'
+        return return_str
+
+    def get_address(self):
+        return str(self.email_addr + self.email_domain)
+    
+    def set_attribute(self, is_valid):
+        self.attrib = is_valid
+
+
+
+class Send_mail:
+    def __init__(self, sender, receivers = [], subject='', body_txt='', images=None, smtp_server=('smtp.gmail.com', 587)):
+        self.sender = sender
+        self.receivers = receivers
+        self.subject = subject
+        self.body_text = body_txt 
+        self.images = images
+        self.smtp_server = smtp_server
+
+    def characteristics(self):
+        return_dict = dict()
+        return_dict['Sender:'] = str(self.sender).strip('-')
+        rec_ls = []
+        for item in self.receivers:
+            if type(item) == str:
+                rec_ls.append(item)
+            else:
+                rec_ls.append(item.email_addr + item.email_domain)
+            
+        return_dict["Receivers:"] = rec_ls
+        return_dict["Subject:"] = str(self.subject)
+        return_dict["Body:"] = self.body_text
+        return return_dict
+    def __str__(self):
+        return_str = '-'*35 + '\n'
+        for key, val in self.characteristics().items():
+            if val != None:
+                return_str += str(key) + ' ' + str(val)
+            else:
+                return_str += str(key) + ' ' +  'N/A'
+            return_str += '\n'
+        return_str += '-'*35 + '\n'
+        return return_str
+
+    def set_sender(self, email_obj=''):
+        self.sender = email_obj
+
+    def set_receivers(self, receivers=[]):
+        self.receivers = receivers
+    
+    def add_receiver(self, email_obj):
+      
+        self.receivers.append(email_obj)
+
+    def send_email(self):
+        
+        search_dict = self.characteristics()
+        curr_email = ''
+        for item in search_dict["Receivers:"]:
+            try:
+                curr_email = item
+                email = MIMEMultipart()
+                email["From"] = self.sender.get_address()
+                email["To"] = item
+                email["Subject"] = self.subject
+                password = self.sender.passw
+                email.attach(MIMEText(self.body_text, "plain"))
+                #start the smtp session
+                #connect to the gmail smtp server using the tcp 587 port 
+                #start a tls session 
+                session = smtplib.SMTP(self.smtp_server[0], self.smtp_server[1])
+                session.starttls()
+                session.login(self.sender.get_address(), password) 
+                self.sender.set_attribute(True)
+                text = email.as_string()
+                print("sending to " + item + '...')
+                session.sendmail(self.sender.get_address(), item, text)
+                session.quit()
+                print('Mail Sent From: ' + self.sender.get_address() + ' To: ' + item)
+            except smtplib.SMTPAuthenticationError:
+                print("sender email username or password invalid")
+                self.sender.set_attribute(False)
+            except smtplib.SMTPRecipientsRefused:
+                print("recipient address invalid")
+                
+
+       
+            
+
+
+
+
+
+
+
+
+reciep_emails = ["maya.ktu420@gmail.com", '']
+
+my_email = Email('testmc777', passw='Poop6969!')
+
+new_email = Send_mail(my_email, reciep_emails, "You Free Later? ;)", 'https://play.typeracer.com?rt=1vf2teqqhl')
+
+new_email.send_email()
+
